858f7c9a9a49f28a2b0b3f3faf2de536e3e87e0e
seriksk1 <seriksk1@ukr.net>
2022-01-26 22:17:48 +0200

    Feature: Display fight log in div

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ function getElById(id) {
 
 const btn = getElById('btn-kick');
 const btn2 = getElById('btn-kick-2');
+const $logs = document.querySelector('#logs');
 
 const character = {
     name: 'Pikachu',
@@ -47,7 +48,13 @@ function changeHP(count) {
 
     const log = this === enemy ? generateLog(this, character) : generateLog(this, enemy);
 
-    console.log(log + `(-${count}HP [${this.damageHP}/${this.defaultHp}])`);
+    $logs.scrollTop = 0;
+
+    const $p = document.createElement('p');
+    $p.innerText = `${log} (-${count}HP [${this.damageHP}/${this.defaultHp}])`;
+    $logs.insertBefore($p, $logs.children[0]);
+
+    // console.log(`${log} (-${count}HP [${this.damageHP}/${this.defaultHp}])`);
 
     if (this.damageHP <= 0) {
         this.damageHP = 0;
@@ -77,18 +84,18 @@ btn2.addEventListener('click', function () {
     enemy.changeHP(random(5));
 })
 
-function generateLog(firstPerson, secondPerson) {
+function generateLog({name: first}, {name: second}) {
     const logs = [
-        `${firstPerson.name} вспомнил что-то важное, но неожиданно ${secondPerson.name}, не помня себя от испуга, ударил в предплечье врага.`,
-        `${firstPerson.name} поперхнулся, и за это ${secondPerson.name} с испугу приложил прямой удар коленом в лоб врага.`,
-        `${firstPerson.name} забылся, но в это время наглый ${secondPerson.name}, приняв волевое решение, неслышно подойдя сзади, ударил.`,
-        `${firstPerson.name} пришел в себя, но неожиданно ${secondPerson.name} случайно нанес мощнейший удар.`,
-        `${firstPerson.name} поперхнулся, но в это время ${secondPerson.name} нехотя раздробил кулаком \<вырезанно цензурой\> противника.`,
-        `${firstPerson.name} удивился, а ${secondPerson.name} пошатнувшись влепил подлый удар.`,
-        `${firstPerson.name} высморкался, но неожиданно ${secondPerson.name} провел дробящий удар.`,
-        `${firstPerson.name} пошатнулся, и внезапно наглый ${secondPerson.name} беспричинно ударил в ногу противника.`,
-        `${firstPerson.name} расстроился, как вдруг, неожиданно ${secondPerson.name} случайно влепил стопой в живот соперника.`,
-        `${firstPerson.name} пытался что-то сказать, но вдруг, неожиданно ${secondPerson.name} со скуки, разбил бровь сопернику.`
+        `${first} вспомнил что-то важное, но неожиданно ${second}, не помня себя от испуга, ударил в предплечье врага.`,
+        `${first} поперхнулся, и за это ${second} с испугу приложил прямой удар коленом в лоб врага.`,
+        `${first} забылся, но в это время наглый ${second}, приняв волевое решение, неслышно подойдя сзади, ударил.`,
+        `${first} пришел в себя, но неожиданно ${second} случайно нанес мощнейший удар.`,
+        `${first} поперхнулся, но в это время ${second} нехотя раздробил кулаком \<вырезанно цензурой\> противника.`,
+        `${first} удивился, а ${second} пошатнувшись влепил подлый удар.`,
+        `${first} высморкался, но неожиданно ${second} провел дробящий удар.`,
+        `${first} пошатнулся, и внезапно наглый ${second} беспричинно ударил в ногу противника.`,
+        `${first} расстроился, как вдруг, неожиданно ${second} случайно влепил стопой в живот соперника.`,
+        `${first} пытался что-то сказать, но вдруг, неожиданно ${second} со скуки, разбил бровь сопернику.`
     ];
 
     return logs[random(logs.length) - 1];
